ae1ecf749bfef22357e6575ff03d009763267852
Shahbaj Singh <ssingh@comqi.com>
2024-05-11 21:36:00 -0400

    added model for Users collection

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,12 @@ const router = app.Router();
 router.post('/login',(req,res)=>{
     let username = req.body.username;
     let password =  req.body.password;
-    res.send("Welcome "+username);
+    res.send("Welcome back, "+username);
 });
 
+router.post('/register',(req,res)=>{
+    let username = req.body.username;
+    let password =  req.body.password;
+    res.send("Registered "+username);
+});
 module.exports = router;
