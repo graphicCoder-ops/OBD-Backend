aa571f475fe4d9b4048bf9df107b418787f93413
Shahbaj Singh <ssingh@comqi.com>
2024-05-13 11:18:39 -0400

    auth/register updated!

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const app = require("express");
 const router = app.Router();
+const User =  require('../models/user');
 
 router.post('/login',(req,res)=>{
     let username = req.body.username;
@@ -7,9 +8,21 @@ router.post('/login',(req,res)=>{
     res.send("Welcome back, "+username);
 });
 
-router.post('/register',(req,res)=>{
-    let username = req.body.username;
-    let password =  req.body.password;
-    res.send("Registered "+username);
+router.post('/register',async (req,res)=>{
+    const existingUser = await User.findOne({username:req.body.username});
+    
+    if(!existingUser){
+    try {
+        const newUser = new User({username:req.body.username,password:req.body.password});
+        await newUser.save();
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error registering user');
+    }
+}else{
+    res.status(400).send('Username already exists');
+}
+    res.send("Registered "+req.body.username);
 });
+
 module.exports = router;
