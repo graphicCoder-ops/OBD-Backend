e73480ca0e678a1d4fd000cd1e502ec5ed2a4a31
Shahbaj Singh <ssingh@comqi.com>
2024-05-31 16:54:17 -0400

    comments for teerth

--- routes/sensor.js
+++ routes/sensor.js
@@ -1,16 +1,22 @@
 const app = require("express");
 const router = app.Router();
+
 //const SensorData = require('../models/sensordata');
 
 router.get('/getData',(req,res)=>{
+    // if user exists get data
     res.send("Function not implemented yet");
 });
 
+
 router.put('/updateData',(req,res)=>{
+    // if username doesnt exist use user initialize
+    // if username exists update all data
     res.send("Function not implemented yet");
 });
 
 router.post('/initialize',(req,res)=>{
+    // if username doesnt exist create one with data
     res.send("Function not implemented yet");
 });
 
