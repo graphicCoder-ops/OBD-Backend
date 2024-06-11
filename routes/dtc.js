93945655d8c13ae6a07c02a6cf905832fc9aa715
Shahbaj Singh <ssingh@comqi.com>
2024-06-10 22:01:22 -0400

    base functionality for DTC codes

--- routes/dtc.js
+++ routes/dtc.js
@@ -2,13 +2,32 @@ const app = require("express");
 const UserDTC = require("../models/dtc");
 const router = app.Router();
 
+
 router.get('/get', async (req, res) => {
-    //const DTC = new UserDTC({username:"Shit",DTCs:['2302','3453','3457','3dc34']});
-    await DTC.save();
-    res.send("Function not implemented");
+    const user = UserDTC.findOne(); 
+    res.json(user.DTCs);
 });
 
-router.post('/set', (req, res) => {
+router.ws('/ws', async (ws, res) => {
+    ws.on('DTCs', msg => {
+        console.log(msg)
+    });
+
+    // test with client python script
+});
+
+router.post('/set', async (req, res) => {
+    // if user exists update
+    const user = UserDTC.findOne(); 
+    user.DTCs = req.body.DTCs;
+    await user.save();
+    // if not then this
+    const userDTC = new UserDTC(req.body);
+    try {
+        await userDTC.save();
+    } catch (error) {
+        
+    }
     res.send("Function not implemented");
 });
 
