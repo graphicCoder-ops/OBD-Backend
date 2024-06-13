9fa9d490b462074e5f93e34fd06d556cf2d75d91
tirth41 <tirthspatel1116@gmail.com>
2024-06-12 23:41:51 -0400

    dtc

--- routes/dtc.js
+++ routes/dtc.js
@@ -2,12 +2,22 @@ const app = require("express");
 const UserDTC = require("../models/dtc");
 const router = app.Router();
 
-
-router.get('/get', async (req, res) => {
-    const user = UserDTC.findOne(); 
-    res.json(user.DTCs);
+router.get("/get", async (req, res) => {
+  try {
+    const userdtc = await UserDTC.findOne({ username: req.body.username });
+    if (userdtc) {
+      res.status(200);
+      res.json(userdtc);
+    } else {
+      res.status(401).send("Username Doesnt Exist!!!");
+    }
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Error getting DTC Codes!");
+  }
 });
 
+/* 
 router.ws('/ws', async (ws, res) => {
     ws.on('DTCs', msg => {
         console.log(msg)
@@ -15,20 +25,27 @@ router.ws('/ws', async (ws, res) => {
 
     // test with client python script
 });
+*/
+router.post("/set", async (req, res) => {
+  const { username, DTCs } = req.body;
 
-router.post('/set', async (req, res) => {
-    // if user exists update
-    const user = UserDTC.findOne(); 
-    user.DTCs = req.body.DTCs;
-    await user.save();
-    // if not then this
-    const userDTC = new UserDTC(req.body);
+  const userdtc = await UserDTC.findOne({ username: username });
+  // if user exists update
+  if (userdtc) {
     try {
-        await userDTC.save();
+      userdtc.DTCs = req.body.DTCs;
+      await userdtc.save();
+      res.sendStatus(200);
     } catch (error) {
-        
+      console.log(error.message);
+      res.status(500).send("Error adding DTC Codes!");
     }
-    res.send("Function not implemented");
+  } // if not then this
+  else {
+    const newDtc = new UserDTC({ username: username, DTCs: DTCs });
+    await newDtc.save();
+    res.status(200).send("Added new DTC codes");
+  }
 });
 
 module.exports = router;
