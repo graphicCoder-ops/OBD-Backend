8026b6c121e44c8c326a269856d127db87d1da19
tirth41 <tirthspatel1116@gmail.com>
2024-06-19 19:30:05 -0400

    confirm password

--- routes/auth.js
+++ routes/auth.js
@@ -1,58 +1,71 @@
 const app = require("express");
 const router = app.Router();
-const User = require('../models/user');
+const User = require("../models/user");
 
-router.post('/login', async (req, res) => {
-    const {username,password} = req.body;
-    try {
-        const activeUser = await User.findOne({ username: username.toLowerCase(), password: password });
-        if (activeUser) {
-            res.status(200).send("Login Successfully!," + activeUser.username);
-        } else {
-            res.status(401).send("Authentication Failed!");
-        }
-    } catch (error) {
-        res.status(500).send("Server unable to login!");
-    }
-});
-
-router.post('/register', async (req, res) => {
-    const {username,password} = req.body;
-    const existingUser = await User.findOne({ username: username});
-    
-    if (!existingUser) {
-        try {
-            const newUser = new User({ username: username.toLowerCase(), password: password });
-            await newUser.save();
-            res.status(200).send('Registered, '+ newUser.username);
-        } catch (error) {
-            console.error(error);
-            res.status(500).send('Error registering user');
-        }
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const activeUser = await User.findOne({
+      username: username.toLowerCase(),
+      password: password,
+    });
+    if (activeUser) {
+      res.status(200).send("Login Successfully!," + activeUser.username);
     } else {
-        res.status(400).send('Username already exists');
+      res.status(401).send("Authentication Failed!");
     }
+  } catch (error) {
+    res.status(500).send("Server unable to login!");
+  }
 });
 
-router.put('/change-password', async (req, res) => {
-    const { username, newPassword , oldPassword } = req.body;
+router.post("/register", async (req, res) => {
+  const { username, password, confirmPassword } = req.body;
+  const existingUser = await User.findOne({ username: username });
+
+  if (!existingUser) {
     try {
-        const user = await User.findOne({ username:username.toLowerCase(), password:oldPassword});
-        if (!user) {
-            return res.status(401).send('User not found');
-        }
-        else{
-            user.password = newPassword;
-            await user.save();
-            res.status(200).send('Password changed successfully!');
-        }
-
-        
+      const newUser = new User({
+        username: username.toLowerCase(),
+        password: password,
+        confirmPassword: confirmPassword,
+      });
+      
+       if (password == confirmPassword) {
+            await newUser.save();
+            console.log(confirmPassword , password)
+             res.status(200).send("Registered, " + newUser.username);
+       } else {
+          res.status(400).json({ message: "Passwords do not match" });
+         
+      }
     } catch (error) {
-        console.error(error);
-        res.status(500).send('Error changing password');
+      console.error(error);
+      res.status(500).send("Error registering user");
     }
+  } else {
+    res.status(400).send("Username already exists");
+  }
 });
 
+router.put("/change-password", async (req, res) => {
+  const { username, newPassword, oldPassword } = req.body;
+  try {
+    const user = await User.findOne({
+      username: username.toLowerCase(),
+      password: oldPassword,
+    });
+    if (!user) {
+      return res.status(401).send("User not found");
+    } else {
+      user.password = newPassword;
+      await user.save();
+      res.status(200).send("Password changed successfully!");
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error changing password");
+  }
+});
 
 module.exports = router;
