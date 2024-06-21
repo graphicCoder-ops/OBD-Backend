ce9020583392fce8f4dc69065e286ec8eb55b6ef
graphicCoder-ops <ssingh@comqi.com>
2024-06-21 18:16:42 -0400

    sensors api at high level

--- models/sensordata.js
+++ models/sensordata.js
@@ -3,15 +3,15 @@ const mongoose = require("mongoose");
 const sensorSchema = mongoose.Schema({
     timestamp: { type: Date, default: Date.now },
     username: {type: String, required: true},
-    rpm: { type: Number, required: false },
-    speed: { type: Number, required: false },
-    coolantTemp: { type: Number, required: false },
-    throttlePos: { type: Number, required: false },
-    intakeTemp: { type: Number, required: false },
-    maf: { type: Number, required: false },
-    runTime: { type: Number, required: false },
-    fuelLevel: { type: Number, required: false },
-    barometricPressure: { type: Number, required: false }
+    RPM: { type: String, required: false },
+    SPEED: { type: String, required: false },
+    ENGINE_LOAD: { type: String, required: false },
+    LONG_FUEL_TRIM_1: { type: String, required: false },
+    O2_B1S1: { type: String, required: false },
+    THROTTLE_POS: { type: String, required: false },
+    COOLANT_POS: { type: String, required: false },
+    MAF: { type: String, required: false },
+    FUEL_LEVL: { type: String, required: false },
 });
 
 module.exports = mongoose.model('SensorData', sensorSchema);
--- routes/sensor.js
+++ routes/sensor.js
@@ -4,16 +4,39 @@ const router = app.Router();
 
 //const SensorData = require('../models/sensordata');
 
-router.get('/getData',(req,res)=>{
-    // username  is exists
-    res.send("Function not implemented yet");
-});
+router.get("/get/:username", async (req, res) => {
+    try {
+      const sensors = await sensordata.findOne({ username: req.params.username });
+      if (sensors) {
+        res.status(200);
+        res.json(sensors);
+      } else {
+        res.status(401).send("Username Doesnt Exist!!!");
+      }
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Error getting sensors data");
+    }
+  });
 
 
-router.put('/updateData',(req,res)=>{
+router.post('/set', async (req,res)=>{
     // if username doesnt exist create new entry
     // if username exists update all data
-    res.send("Function not implemented yet");
+    const user = await sensordata.findOne({username: req.body.username});
+    if(user){
+        try {
+            const sensor = await sensordata(req.body);
+            sensor.save();
+            res.status(200).send("Saved!");
+        } catch (error) {
+            res.status(500).send("Couldn't save data");
+        }
+    }else{
+        const sensor = await sensordata(req.body);
+        sensor.save();
+        res.status(200).send("Saved!");
+    }
 });
 
 module.exports = router;
